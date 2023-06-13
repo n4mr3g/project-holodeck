8169725e3de05537554386400b8503023221ee18
n4mr3g <german.n.piccioni@gmail.com>
2023-06-13 18:21:10 +0200

    WIP: fetch user messages from database BUG: server crashes if user is logged out

--- server/controllers/messages.controller.js
+++ server/controllers/messages.controller.js
@@ -4,21 +4,52 @@ const { User } = require('../models/messages.model.js');
 
 async function getMessages(req, res) {
   const userId = req.params.userId;
-  console.log('userId', userId);
-  User.findOne({ userId: userId })
-    .then((user) => {
-      if (!user) {
-        res.status(404).send('User not found');
-      }
-      res.status(200).send(user.messages.filter((message) =>
-        message.author !== '[SYSTEM]'
-      ));
-    })
-    .catch((e) => {
-      res.status(200).send('Error getting messages');
-      console.error('Server error: ', e);
-    });
+  if (!userId) {
+    return;
+  }
+  try {
+    User.findOne({ userId: userId })
+      .then((user) => {
+        if (!user) {
+          res.status(404).send('User not found');
+        }
+        res.status(200).send(user.messages.filter((message) =>
+          message.author !== '[SYSTEM]'
+        ));
+      })
+      .catch((e) => {
+        res.status(200).send('Error getting messages');
+        console.error('Server error: ', e);
+      });
+  } catch (e) {
+    console.error('Error: User not logged in.', e);
+    res.status(401).send('User not logged in.');
+  }
 }
+// async function getMessages(req, res) {
+//   const userId = req.params.userId;
+//   if (!userId) {
+//     return;
+//   }
+//   try {
+//     User.findOne({ userId: userId })
+//       .then((user) => {
+//         if (!user) {
+//           res.status(404).send('User not found');
+//         }
+//         res.status(200).send(user.messages.filter((message) =>
+//           message.author !== '[SYSTEM]'
+//         ));
+//       })
+//       .catch((e) => {
+//         res.status(200).send('Error getting messages');
+//         console.error('Server error: ', e);
+//       });
+//   } catch (e) {
+//     console.error('Error: User not logged in.', e);
+//     res.status(401).send('User not logged in.');
+//   }
+// }
 
 module.exports = {
   getMessages,
